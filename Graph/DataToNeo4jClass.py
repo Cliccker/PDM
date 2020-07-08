b6175470e2c45c06f27507d0124a0ba9dc0d96d1
Magenta997 <52920981+Magenta997@users.noreply.github.com>
2020-07-08 17:00:36 +0800

    add

--- Graph/DataToNeo4jClass.py
+++ Graph/DataToNeo4jClass.py
@@ -11,48 +11,48 @@ class DataToNeo4j(object):
         link = Graph("http://localhost:7474", username="neo4j", password="1234")
         self.graph = link
         # 定义label
-        self.entity_name = ["一级标题","二级标题","三级标题","四级标题",]
+        self.entity_name = '实体'
         self.entity_id = '编号'
         self.graph.delete_all()
 
-    def create_node(self, nodeKey, nodeValue, nodeDict):
+    def create_node(self, nodeKey, nodeValue,nodeDict):
         """建立节点"""
-        for key in nodeDict:
-            print(key)
-            Zero = collections.Counter(key)
-            print(Zero['0'])
-            if Zero['0'] == 0:
-                name_node = Node(self.entity_name[3], name=nodeDict[key])
-                self.graph.create(name_node)
-            if Zero['0'] == 1:
-                name_node = Node(self.entity_name[2], name=nodeDict[key])
-                self.graph.create(name_node)
-            if Zero['0'] == 2:
-                name_node = Node(self.entity_name[1], name=nodeDict[key])
-                self.graph.create(name_node)
-            if Zero['0'] == 3:
-                name_node = Node(self.entity_name[0], name=nodeDict[key])
-                self.graph.create(name_node)
-
+        """建立标题节点"""
+        for Key in nodeKey:
+            name_node = Node(self.entity_name, name=Key)
+            self.graph.create(name_node)
+        """建立序号节点"""
         for name in nodeValue:
-            id_node = Node(self.entity_id, name=name)
+            id_node = Node(self.entity_id, name=name,title=nodeDict[name])
             self.graph.create(id_node)
 
-    def create_relation(self, df_data):
+    def create_relation(self, df_data,id_data,nodeDict,fullname_dict):
         """建立联系"""
-
         m = 0
         for m in range(0, len(df_data)):
-            i = 3
-            while i >0:
-                try:
-                    rel = Relationship(self.graph.find_one(label=self.entity_name[i], property_key='name',
-                                                           property_value=df_data['itself'][m]),
-                                       '属于',
-                                       self.graph.find_one(label=self.entity_name[i-1], property_key='name',
-                                                           property_value=df_data['superior'][m]))
-                    self.graph.create(rel)
+            try:
+                rel = Relationship(self.graph.find_one(label=self.entity_name, property_key='name',
+                                                       property_value=df_data['itself'][m]),
+                                   df_data['relation'][m],
+                                   self.graph.find_one(label=self.entity_name, property_key='name',
+                                                       property_value=fullname_dict[df_data['superior'][m]]))
 
-                except AttributeError:
-                    pass
-                i -=1 
+                self.graph.create(rel)
+            except AttributeError as e:
+                print(e, m)
+            except KeyError as ee:
+                print(ee,m)
+        n = 0
+        for n in range(0, len(id_data)):
+            try:
+                rel = Relationship(self.graph.find_one(label=self.entity_id, property_key='name',
+                                                           property_value=id_data['itself'][n]),
+                                       df_data['relation'][n],
+                                       self.graph.find_one(label=self.entity_id, property_key='name',
+                                                           property_value=id_data['superior'][n]))
+
+                self.graph.create(rel)
+            except AttributeError as e:
+                print(e, n)
+            except KeyError as ee:
+                print(ee, n)
--- Graph/title_neo4j.py
+++ Graph/title_neo4j.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-
 from DataToNeo4jClass import DataToNeo4j
-from py2neo import Node, Graph, Relationship
-import os
 import pandas as pd
 
-
 # 提取excel表格中数据，将其转换成dateframe类型
-
 standard_data = pd.read_excel('./Data/demo.xlsx', header=0, encoding='utf8')
 print(standard_data)
 
@@ -16,11 +12,22 @@ def data_extraction():
 
     # 取出标题到list
     node_list_id = []
+    node_list_value = []
+    fullname_dict ={}
     nodeDict = {}
     for i in range(0, len(standard_data)):
-        node_list_id.append(standard_data['序号'][i])
-        nodeDict[standard_data['序号'][i]] = standard_data['标题'][i]
+        id = standard_data['序号'][i]
+        title = standard_data['标题'][i]
+        node_list_id.append(id)
+        node_list_value.append(title + "_" + id)
+        fullname_dict[id] = title + "_" + id
+        nodeDict[id] = title
+        nodeDict['ASME'] = "ASME标准"
 
+    return node_list_value, node_list_id, nodeDict, fullname_dict
+
+
+"""
     # value抽出作node
     node_list_value = []
     for i in range(0, len(standard_data)):
@@ -28,41 +35,51 @@ def data_extraction():
             # 取出表头名称
             node_list_value.append(standard_data[standard_data.columns[n]][i])
     node_list_value = list(set(node_list_value))
-    return  node_list_value ,node_list_id,nodeDict
+    node_list_value = [incom for incom in node_list_value if str(incom) != 'nan']  # 去除空值
+"""
 
 
 def relation_extraction():
     """联系数据抽取"""
-    links_dict ={}
+    links_dict = {}
+    id_dict = {}
+
+    relation_list = []
     id_list = []
+    title_list = []
     entity_list = []
 
     for i in range(0, len(standard_data)):
         m = 1
+        n = 0
         name_node = standard_data[standard_data.columns[m]][i]
-        while m < len(standard_data.columns)-1:
+        id_node = standard_data[standard_data.columns[n]][i]
+        while m < len(standard_data.columns) - 1:
+            relation_list.append(standard_data.columns[m + 1])
             entity_list.append(standard_data[standard_data.columns[m + 1]][i])
-            id_list.append(name_node)
+            title_list.append(name_node + '_' + id_node)
+            id_list.append(id_node)
             m += 1
-
+            n += 1
 
     # 整合数据，将两个list整合成一个dict
-    links_dict['itself'] = id_list
+    links_dict['itself'] = title_list
+    links_dict['relation'] = relation_list
     links_dict['superior'] = entity_list
+    
+    id_dict['itself'] = id_list
+    id_dict['relation'] = relation_list
+    id_dict['superior'] = entity_list
     # 将数据转成DataFrame
-    df_data = pd.DataFrame(links_dict)
-    return df_data
-print(data_extraction())
-print(relation_extraction())
+    df_data = pd.DataFrame(links_dict).dropna(axis=0, how='any').reset_index(drop=True)
+    id_data = pd.DataFrame(id_dict).dropna(axis=0, how='any').reset_index(drop=True)
+    print(df_data)
+    return df_data, id_data
+
+
 create_data = DataToNeo4j()
-create_data.create_node(data_extraction()[0], data_extraction()[1],data_extraction()[2])
-create_data.create_relation(relation_extraction())
-"""
-# 实例化对象
-data_extraction()
-relation_extraction()
-
-
-
-
-"""
+Da = data_extraction()
+Re = relation_extraction()
+create_data.create_node(Da[0], Da[1],Da[2])
+create_data.create_relation(Re[0], Re[1], Da[2],Da[3])
+print("finished")
