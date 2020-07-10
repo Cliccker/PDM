f14efd2962ee83e29e5da27a3a4907402eda6ab9
Magenta997 <52920981+Magenta997@users.noreply.github.com>
2020-07-10 10:56:44 +0800

    update

--- Graph/DataToNeo4jClass.py
+++ Graph/DataToNeo4jClass.py
@@ -15,26 +15,25 @@ class DataToNeo4j(object):
         """建立序号节点"""
         n = 0
         for name in nodeValue:
-            id_node = Node(name[0], name=name[1], title=nodeDict[name[1]][1])
+            id_node = Node(name[0], title=name[1], name=nodeDict[name[1]][1])
             self.graph.create(id_node)
             n += 1
         print("{} Nodes Created".format(n))
 
-
     def create_relation(self, id_data, nodeDict):
         """建立联系"""
-        count =0
+        count = 0
         for n in range(0, len(id_data)):
             try:
-                rel = Relationship(self.graph.find_one(label=nodeDict[id_data['itself'][n]][0], property_key='name',
+                rel = Relationship(self.graph.find_one(label=nodeDict[id_data['itself'][n]][0], property_key='title',
                                                        property_value=id_data['itself'][n]),
                                    id_data['relation'][n],
-                                   self.graph.find_one(label=nodeDict[id_data['superior'][n]][0], property_key='name',
+                                   self.graph.find_one(label=nodeDict[id_data['superior'][n]][0], property_key='title',
                                                        property_value=id_data['superior'][n]))
                 self.graph.create(rel)
-                count +=1
+                count += 1
             except AttributeError as e:
                 print(e, n)
-            except KeyError as Ke:
+            except KeyError :
                 pass
         print("{} Relationships Created".format(count))
